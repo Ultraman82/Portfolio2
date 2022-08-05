14411ada48c32be1d083c299343d951687d72f72
Ultraman82 <exelcior99@gmail.com>
2022-08-05 09:47:16 -0400

    0805

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-(function () {
+(
+    function sendMail() {
+        $.ajax({
+          type: 'POST',
+          url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+          data: {
+            'key': 'YOUR API KEY HERE',
+            'message': {
+              'from_email': 'YOUR@EMAIL.HERE',
+              'to': [
+                  {
+                    'email': 'RECIPIENT@EMAIL.HERE',
+                    'name': 'RECIPIENT NAME (OPTIONAL)',
+                    'type': 'to'
+                  }
+                ],
+              'autotext': 'true',
+              'subject': 'YOUR SUBJECT HERE!',
+              'html': 'YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!'
+            }
+          }
+         }).done(function(response) {
+           console.log(response); // if you're into that sorta thing
+         });
+    },
+    function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
             document.querySelector(".active-btn").classList.remove("active-btn");
@@ -10,4 +35,8 @@
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
-})();
+}
+
+
+
+)();

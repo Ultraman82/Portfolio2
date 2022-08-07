364a827dca2140960523071f6cee3c3fac1d8bca
Ultraman82 <exelcior99@gmail.com>
2022-08-07 14:22:43 -0400

    second

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-(
-    function sendMail() {
-        $.ajax({
-          type: 'POST',
-          url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-          data: {
-            'key': 'YOUR API KEY HERE',
-            'message': {
-              'from_email': 'YOUR@EMAIL.HERE',
-              'to': [
-                  {
-                    'email': 'RECIPIENT@EMAIL.HERE',
-                    'name': 'RECIPIENT NAME (OPTIONAL)',
-                    'type': 'to'
-                  }
-                ],
-              'autotext': 'true',
-              'subject': 'YOUR SUBJECT HERE!',
-              'html': 'YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!'
-            }
-          }
-         }).done(function(response) {
-           console.log(response); // if you're into that sorta thing
-         });
-    },
+(  
     function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
@@ -34,9 +10,10 @@
     });
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
-    })
+    })    
+    
+    document.querySelector(".submit-btn").addEventListener("click", function () {
+      document.getElementById("mail-form").submit();
+    });
 }
-
-
-
 )();
